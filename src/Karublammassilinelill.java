c24d2e8c05f3ebde1a6832dd8495443bf69c795b
Raif The Mad <Raif The Mad@172.30.12.21>
2017-09-20 14:02:18 +0300

    Läheb käima!

---
+++ src/Karublammassilinelill.java
@@ -0,0 +1,12 @@
+
+public class Karublammassilinelill {
+
+	public static void main(String[] args) {
+		String karu="Ka karu ju ei pese end kuid seegipoolest tugevaks ta j‰‰b!";
+		int rubla=616;
+		String ılu="ıllet";
+		System.out.println(karu+rubla+ılu);
+
+	}
+
+}
